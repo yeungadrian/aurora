a4a94f1edcd574bfd4c40cb50dc1bba87dfde2c5
Adrian Yeung <yeungadrian@Adrians-MacBook-Air.local>
2020-06-14 14:34:56 +0100

    Adding endpoint to get mean variance for funds

--- app/routers/efficientFrontier.py
+++ app/routers/efficientFrontier.py
@@ -89,10 +89,10 @@ def simulatedFrontier(item: simulatedFrontierItem):
 
     def get_annual_portfolio_performance(weights, mean_returns, cov_matrix):
         returns = np.sum(mean_returns * weights ) * 252
-        stdev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
-        return stdev, returns
+        volatility = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
+        return volatility, returns
 
-    def generate_random_portfolios(num_portfolios, num_funds,mean_returns, cov_matrix, risk_free):
+    def generate_random_portfolios(num_portfolios, num_funds, mean_returns, cov_matrix, risk_free):
         results = np.zeros((3,num_portfolios))
         weights_List = []
         for i in range(num_portfolios):
@@ -115,7 +115,7 @@ def simulatedFrontier(item: simulatedFrontierItem):
     results, weights = generate_random_portfolios(num_portfolios,num_funds,mean_returns, cov_matrix, risk_free)
 
     results_DF = pd.DataFrame(results).transpose()
-    results_DF.columns = ['std_dev','return','sharpe_ratio']
+    results_DF.columns = ['Volatility','Return','Sharpe Ratio']
 
     weightsDF = pd.DataFrame(weights)
     weightsDF.columns = codeList
@@ -158,8 +158,8 @@ def EfficientFrontier(item: efficientFrontierItem):
 
     def get_annual_portfolio_performance(weights, mean_returns, cov_matrix):
         returns = np.sum(mean_returns * weights ) * 252
-        stdev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
-        return stdev, returns
+        volatility = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
+        return volatility, returns
 
     returns = indexData[returnList]
     returns.columns = codeList
@@ -203,14 +203,81 @@ def EfficientFrontier(item: efficientFrontierItem):
     
     min_vol = min_variance(mean_returns, cov_matrix, num_funds)
     mid_stdev, min_return = get_annual_portfolio_performance(min_vol['x'], mean_returns, cov_matrix)
-  
-    target = np.linspace(min_return, max(mean_returns)* 252, 50)
+    num_efficient_portfolios = 50
+    target = np.linspace(min_return, max(mean_returns)* 252, num_efficient_portfolios)
 
     efficient_portfolios = efficient_frontier(mean_returns, cov_matrix, target)
 
     output = pd.DataFrame(efficient_portfolios)
     output = output[['x','fun']]
     output['target'] = pd.DataFrame(target)
+    output.columns = ['weights','Volatility','Return']
+    output['Sharpe Ratio'] = (output['Return'] - risk_free) / output['Volatility']
+
+    for y in range(0,len(codeList)):
+        temporaryList = []
+        for x in range(0,num_efficient_portfolios):
+            temporaryList.append(output['weights'][x][y])
+        output[codeList[y]] = temporaryList
+
+    del output['weights']
+    
     output = output.to_json()
 
     return json.loads(output)
+
+@router.post("/meanvariance")
+def EfficientFrontier(item: efficientFrontierItem):
+    json_request = item.dict()
+    codeList = json_request['codelist']
+    start_date = json_request['start_date']
+    end_date = json_request['end_date']
+
+    indexData = pd.read_csv('data/output.csv')
+
+    indexColumns = ['date']+codeList
+    indexData = indexData[indexColumns]
+ 
+    indexData = indexData[indexData['date'] >= start_date]
+    indexData = indexData.sort_values(by='date').reset_index(drop=True)
+
+    for x in range(0,len(codeList)):
+        indexData[f'{codeList[x]}returns'] = indexData[f'{codeList[x]}'].pct_change()
+    
+    indexDataAltair = pd.DataFrame()
+    returnList = []
+
+    for x in range(0,len(codeList)):
+        tempAltair = indexData[['date',codeList[x],f'{codeList[x]}returns']]
+        tempAltair.columns = ['date','price','returns']
+        tempAltair['symbol'] = codeList[x]
+        indexDataAltair = indexDataAltair.append(tempAltair)
+        returnList.append(f'{codeList[x]}returns')
+
+    indexDataAltair = indexDataAltair.reset_index(drop=True)
+
+    returns = indexData[returnList]
+    returns.columns = codeList
+    cov_matrix = returns.cov()
+
+    volatilitylist = {}
+    for y in range(0,len(codeList)):
+        listofzeros = [0] * len(codeList)
+        listofzeros[y] = 1
+        listofzeros = np.array(listofzeros)
+        volatility_temp = np.sqrt(np.dot(listofzeros.T, np.dot(cov_matrix, listofzeros))) * np.sqrt(252)
+        volatilitylist[codeList[y]] = volatility_temp
+
+    mean_returns = returns.mean() * 252
+    mean_returns = pd.DataFrame(mean_returns).reset_index()
+    mean_returns.columns = ['index','Return']
+    volatility = pd.DataFrame(volatilitylist, index = [0]).transpose().reset_index()
+
+    volatility.columns = ['index','Volatility']
+    output = pd.concat([mean_returns,volatility],axis = 1, sort = False)
+
+    output.columns = ['Symbol','Return','Index','Volatility']
+
+    del output['Index']
+
+    return output
