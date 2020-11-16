86112d2c2bc86d816702a5702d984ab02969833c
yeungadrian <yeungadrinajy@gmail.com>
2020-11-16 23:12:02 +0000

    Apply black formatting

--- app/schemas/backtest.py
+++ app/schemas/backtest.py
@@ -5,7 +5,7 @@ class backtest(BaseModel):
     startDate: str
     endDate: str
     portfolio: list
-    benchmark: str= None
+    benchmark: str = None
     strategy: dict
 
     class Config:
@@ -14,19 +14,10 @@ class backtest(BaseModel):
                 "startDate": "2018-12-31",
                 "endDate": "2020-06-30",
                 "portfolio": [
-                    {
-                        "fund": "MMM",
-                        "amount": 1000
-                    },
-                    {
-                        "fund": "ABT",
-                        "amount": 1000
-                    }
+                    {"fund": "MMM", "amount": 1000},
+                    {"fund": "ABT", "amount": 1000},
                 ],
                 "benchmark": "AAPL",
-                "strategy": {
-                    "rebalance": True,
-                    "rebalanceFrequency": "Y"
-                }
+                "strategy": {"rebalance": True, "rebalanceFrequency": "Y"},
             }
         }
