2908bf89135c11f98578798f1d5155e187f9ccda
Adrian Yeung <yeungadrian@Adrians-MacBook-Air.local>
2020-11-16 22:59:50 +0000

    Add loading data to endpoint

--- app/api/data/data_loader.py
+++ app/api/data/data_loader.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 
 
-def loadHistoricalData(fund_codes, startDate, endDate):
+def loadHistoricalData(fund_codes, start_date, end_date):
     fund_codes.insert(0, "date")
-    all_historical_prices = pd.read_csv("output.csv")
+    all_historical_prices = pd.read_csv("app/api/data/output.csv")
     subset_data = all_historical_prices[fund_codes][
-        all_historical_prices["date"] >= startDate
-    ][all_historical_prices["date"] <= endDate]
+        all_historical_prices["date"] >= start_date
+    ][all_historical_prices["date"] <= end_date]
     subset_data = subset_data.sort_values("date").reset_index(drop=True)
     return json.loads(subset_data.to_json(orient="records"))
--- app/api/endpoints/backtest.py
+++ app/api/endpoints/backtest.py
@@ -2,13 +2,21 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from app import schemas
-from app.api.data import loadHistoricalData
+from app.api.data.data_loader import *
 
 router = APIRouter()
 
 
 @router.post("/")
 def backtest(item: schemas.backtest):
-
-    historicalData = loadHistoricalData()
-    return "asd"
+    json_request = item.dict()
+    portfolio = json_request["portfolio"]
+    fund_codes = []
+    for i in portfolio:
+        fund_codes.append(i["fund"])
+    start_date = json_request["startDate"]
+    end_date = json_request["endDate"]
+    historicalData = loadHistoricalData(
+        fund_codes=fund_codes, start_date=start_date, end_date=end_date
+    )
+    return historicalData
