cebd1262d64685e1da2648d226549c485644c298
Adrian Yeung <yeungadrian@Adrians-MacBook-Air.local>
2020-11-16 23:06:29 +0000

    Adding example request to backtest schema

--- app/schemas/backtest.py
+++ app/schemas/backtest.py
@@ -5,5 +5,28 @@ class backtest(BaseModel):
     startDate: str
     endDate: str
     portfolio: list
-    benchmark: str
+    benchmark: str= None
     strategy: dict
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "startDate": "2018-12-31",
+                "endDate": "2020-06-30",
+                "portfolio": [
+                    {
+                        "fund": "MMM",
+                        "amount": 1000
+                    },
+                    {
+                        "fund": "ABT",
+                        "amount": 1000
+                    }
+                ],
+                "benchmark": "AAPL",
+                "strategy": {
+                    "rebalance": True,
+                    "rebalanceFrequency": "Y"
+                }
+            }
+        }
