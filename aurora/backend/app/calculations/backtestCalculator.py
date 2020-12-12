ea90d4b636fe841c5cb2a99e5998b7dc7c0e6c85
yeungadrian <yeungadrianjy@gmail.com>
2020-12-12 16:20:38 +0000

    Fix rebalance duplciation issue and make more robust to dates

--- aurora/backend/app/calculations/backtestCalculator.py
+++ aurora/backend/app/calculations/backtestCalculator.py
@@ -22,7 +22,6 @@ def backtest_funds(
 
     return result
 
-
 def backtest_strategy(historical_index, portfolio, strategy, start_date, end_date):
     fund_index = pd.DataFrame(historical_index)
     fund_codes = []
@@ -35,27 +34,28 @@ def backtest_strategy(historical_index, portfolio, strategy, start_date, end_dat
 
     if strategy["rebalance"]:
         fund_percentage = [i / sum(fund_amount) for i in fund_amount]
+
         frequency = {"y": 12, "q": 3, "m": 1}
-        start_period = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        delta_months = relativedelta(end_date, start_period)
 
-        # To Do: Test edge cases of the rebalancing / dates, e.g. Leap Years, Weekends
+        rebalanceFrequency = frequency[strategy["rebalanceFrequency"].lower()]
 
-        diff_months = delta_months.months + delta_months.years * 12
-        if delta_months.days != 0:
-            diff_months = diff_months + 1
+        fund_index['date'] = pd.to_datetime(fund_index['date'])
 
-        rebalanceFrequency = strategy["rebalanceFrequency"].lower()
+        month_end_dates = fund_index[fund_index['date'].dt.is_month_end]['date'].reset_index(drop = True)
+        rebalancing_dates = month_end_dates[month_end_dates.index % rebalanceFrequency == 0].reset_index(drop=True)
 
         temp_amount = fund_amount
+
         subset_list = []
-        for j in range(0, diff_months):
-            end_period = start_period + relativedelta(
-                months=frequency[rebalanceFrequency]
-            )
-            start_period = start_period.strftime("%Y-%m-%d")
-            end_period = end_period.strftime("%Y-%m-%d")
+
+        for j in range(0, len(rebalancing_dates)):
+
+            start_period = rebalancing_dates[j]
+
+            if j == (len(rebalancing_dates)-1):
+                end_period = end_date
+            else:
+                end_period = rebalancing_dates[j+1]
 
             subset_index = fund_index[fund_index["date"] >= start_period][
                 fund_index["date"] <= end_period
@@ -64,21 +64,22 @@ def backtest_strategy(historical_index, portfolio, strategy, start_date, end_dat
                 subset_index[k] = (
                     subset_index[k] / subset_index[k][subset_index.index[0]]
                 )
-            temp_dates = subset_index["date"]
 
             temp_df = backtest_funds(temp_amount, fund_codes, subset_index)
-            temp_total = temp_df.iloc[(temp_df.shape[0] - 1)].sum()
-            temp_amount = [temp_total * i for i in fund_percentage]
-            temp_df["date"] = temp_dates
 
-            start_period = datetime.strptime(
-                temp_df["date"][temp_df.index[-1]], "%Y-%m-%d"
-            )
+            # Getting the portfolio at the enddate
+            temp_total = temp_df.iloc[(temp_df.shape[0] - 1)][fund_codes].sum()
+
+            temp_amount = [temp_total * i for i in fund_percentage]
+
+            temp_df["date"] = subset_index["date"]
 
             temp_df.drop(temp_df.tail(1).index, inplace=True)
+            
             subset_list.append(temp_df)
 
         result_df = pd.concat(subset_list)
+        result_df['date'] = result_df['date'].dt.strftime('%Y-%m-%d')
 
     else:
         result_df = backtest_funds(fund_amount, fund_codes, fund_index)
