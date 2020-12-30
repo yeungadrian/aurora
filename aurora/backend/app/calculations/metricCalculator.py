7f11b1c54c6c8146e015e917525f666116fe5632
yeungadrian <yeungadrianjy@gmail.com>
2020-12-30 20:59:00 +0000

    Adding additional metrics

--- aurora/backend/app/calculations/metricCalculator.py
+++ aurora/backend/app/calculations/metricCalculator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import json
 
 
 def calculate_metrics(portfolio):
@@ -11,23 +12,50 @@ def calculate_metrics(portfolio):
 
     start_date = portfolio["date"][0]
     end_date = portfolio["date"].iat[-1]
+
+    # Number of years calculation could be improved, do difference in months and pretend always at end of month
     num_years = (end_date - start_date).days / 365.25
+
+    # Needs to be dynamically loaded eventually
+    risk_free = 0.01
 
     month_end_projection = portfolio[portfolio["date"].dt.is_month_end].reset_index(
         drop=True
     )
+
     month_end_projection["monthlyReturns"] = (
-        month_end_projection["portfolio"] / month_end_projection["portfolio"].shift(-1)
+        month_end_projection["portfolio"].shift(1) / month_end_projection["portfolio"]
         - 1
     )
+
+    monthly_returns = month_end_projection[["date", "monthlyReturns"]].dropna(
+        axis="index"
+    )
+    monthly_returns["date"] = monthly_returns["date"].dt.strftime("%Y-%m-%d")
+    monthly_returns = json.loads(monthly_returns.to_json(orient="records"))
 
     cagr = calculate_cagr(
         end_value=end_value, start_value=start_value, num_years=num_years
     )
-    
+    negative_returns = month_end_projection[month_end_projection["monthlyReturns"] < 0]
     monthly_std = calculate_std(returns=month_end_projection["monthlyReturns"])
+    negative_std = calculate_std(returns=negative_returns["monthlyReturns"])
 
-    result = {"cagr": cagr, "monthly_std": monthly_std}
+    sharpe_ratio = calculate_portfolio_ratio(
+        portfolio_return=cagr, risk_free=risk_free, std=monthly_std
+    )
+
+    sortino_ratio = calculate_portfolio_ratio(
+        portfolio_return=cagr, risk_free=risk_free, std=negative_std
+    )
+
+    result = {
+        "cagr": cagr,
+        "monthly_std": monthly_std,
+        "sharpe_ratio": sharpe_ratio,
+        "sortino_ratio": sortino_ratio,
+        "monthly_returns": monthly_returns,
+    }
 
     return result
 
@@ -40,3 +68,7 @@ def calculate_cagr(end_value, start_value, num_years):
 def calculate_std(returns):
 
     return np.std(returns)
+
+
+def calculate_portfolio_ratio(portfolio_return, risk_free, std):
+    return (portfolio_return - risk_free) / std
